c720d89b884734dca9456bc20d587f1db04a50c4
SOMRAJ <75350513+iam-somraj@users.noreply.github.com>
2025-08-23 18:40:48 +0530

    Create first.c

---
+++ Day-02/first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int number = 42;       //it store integers
+    float price = 10.5f;   //it store single-precision floating-point numbers
+    double pi = 3.14159;    //it store double-precision floating-point numbers
+    char grade = 'A';    //it store character 
+    
+    return 0;
+}
